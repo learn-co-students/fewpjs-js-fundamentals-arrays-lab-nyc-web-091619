16fb2e03c96bfc92079173414c660dac5380dda5
github username <github email address>
2019-11-12 09:09:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+// let append = function (pet) {
+//     return Array.push(pet)
+// }
+
+const append = ["Milo", "Otis", "Garfield",]
+append.push("Odie")
+const prepend = ["Milo", "Otis", "Garfield",]
+prepend.unshift("Odie")
+const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift()
